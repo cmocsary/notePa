dc8ce051382bc463c1bccebec08c3e169b61da97
Carlos Mocsary <carlos.mocsary@gmail.com>
2022-01-19 02:42:33 -0300

    avance con tablero de notas, y funcion mostrar y guardar notas nuevas

--- js/index.js
+++ js/index.js
@@ -1,49 +1,20 @@
-class User {
-    constructor(id, email, pass){
-        this.id = id;
-        this.email = email;
-        this.pass = pass;
-    }
-}
-
-let users = [
-    new User(
-        1,
-        'carlos.mocsary@gmail.com',
-        '1234'
-    ),
-    new User(
-        2,
-        'maflorencia.norry@gmail.com',
-        '5612'
-    )
-]
-
-if(!localStorage.getItem('users')){
-    let localS = JSON.stringify(users);
-    localStorage.setItem('users', localS);
-}
-
-const loginValidation = (e)=>{
-    e.preventDefault()
-
+const loginValidation = ()=>{
     
+}
 
-
-    let localS = localStorage.getItem('users');
-    let getLS = JSON.parse(localS)
-
+const login = async (e)=> {
+    e.preventDefault();
     let email = document.getElementById('inputEmail');
     let pass = document.getElementById('inputPassword');
-    pass.toString();
-
-    getLS.forEach(user => {
+    const response = await fetch('http://localhost:3000/users')
+    const data = await response.json();
+    data.forEach(user => {
         if(user.email === email.value && user.pass === pass.value){
-            console.log("valido")
+            let localS = JSON.stringify(user);
+            localStorage.setItem('user', localS);
             window.location.assign(window.location.origin + "/main.html");
         }else{
             console.log("no valida")
         }
     });
 }
-
--- js/main.js
+++ js/main.js
@@ -1,25 +1,48 @@
-class Note{
-    constructor(id, description){
-        this.id = id;
-        this.description = description;
-    }
-};
-
-let words = document.getElementById("wordsContent")
-let note = document.getElementById("boxNote")
-words.addEventListener('keyup', ()=>{
-    note.innerHTML = words.value;
+let words = document.getElementById("wordsContent");
+let note = document.getElementById("boxNote");
+words.addEventListener("keyup", () => {
+  note.innerHTML = words.value;
 });
 
-const addNote = () =>{
+const showNotes = () => {
+  let localS = localStorage.getItem("user");
+  let getLS = JSON.parse(localS);
+  
+  getLS.notes.forEach(note => {
+      let cardNote = document.createElement("div");
+      cardNote.innerHTML = `
+            <div class="m-2 card">
+                <div class="card-body">     
+                    <h5 class="d-flex justify-content-between card-title">Card title <a href="deleteNote()"><i class="bg-danger text-white btnDelete p-1 fas fa-trash-alt"></i></a></h5>
+                    <hr>
+                    <p class="card-text">${note}</p>
+                </div>
+            </div> 
+        `;
+        cardNote.className = "col col-sm-12 col-md-12 col-lg-6 ";
+        let noteBoard = document.getElementById('noteBoard')
+        noteBoard.appendChild(cardNote);
+  });
+};
 
-}
+showNotes();
 
-const deleteNote = () =>{
+const addNote = async (e) => {
+  e.preventDefault();
 
-}
+  let localS = localStorage.getItem("user");
+  let getLS = JSON.parse(localS);
+  let dataNote = document.getElementById('wordsContent')
+  getLS.notes.append(dataNote.value)
+  await fetch("http://localhost:3000/users" + getLS.id, {
+    method: "PUT",
+    headers: {
+      "Content-Type":"application/json",
+    },
+    body: JSON.stringify(getLS),
+  });
+};
 
-const updateNote = () =>{
+const deleteNote = () => {};
 
-}
-
+const updateNote = () => {};
